3dcaf6723a8cfd4ff965a7b62223fa01fbd5f85d
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-12-09 12:27:33 +0800

    Refractored MIPSController to be just a controller The simulation stuff goes in Simulator.java

--- src/simulator/MIPSController.java
+++ src/simulator/MIPSController.java
@@ -3,24 +3,17 @@ package simulator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.math.BigInteger;
-import java.util.Map.Entry;
-
-import util.ByteUtils;
-import architecture.Memory;
-import architecture.Registers;
 
 public class MIPSController {
 	public static void main(String[] args) {
 		new MIPSController();
 	}
 
-	private Memory memory;
-	private Registers registers;
+	
+	private Simulator simulator;
 
 	public MIPSController() {
-		this.memory = new Memory();
-		this.registers = new Registers();
+		this.simulator = new Simulator();
 
 		// Default file
 		// Debugging only
@@ -33,80 +26,11 @@ public class MIPSController {
 
 		Program program = new Program(document);
 
-		loadProgram(program);
-		
+		this.simulator.loadProgram(program);
+
 		// For debugging
-		this.registers.seeRegisters();
-		this.memory.seeMemory();
+//		this.registers.seeRegisters();
+//		this.memory.seeMemory();
 	}
 
-	private void loadProgram(Program program) {
-		
-		// Load registers
-		for (Entry<String, Object> entry : program.getRegisters().entrySet()) {
-			String register = entry.getKey();
-			Long value = 0L;
-
-			// We need because YAML loads data based on their size, but we
-			// ALWAYS need a Long value.
-			if (entry.getValue() instanceof Integer)
-				value = new Long((Integer) entry.getValue());
-			else if (entry.getValue() instanceof Long)
-				value = (Long) entry.getValue();
-			else if (entry.getValue() instanceof BigInteger)
-				value = ((BigInteger) entry.getValue()).longValue();
-
-			try {
-				this.registers.setRegister(
-						Integer.parseInt(register.replaceAll("R(\\d+)", "$1")),
-						value);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		// Load memory
-		for (Entry<Integer, Byte> entry : program.getMemory().entrySet()) {
-			this.memory.setMemoryAddress(entry.getKey(), entry.getValue());
-		}
-		
-		// TODO: Place code that saves the code to the code segment here
-		// You can get the code in program.code
-		// int lineNumber = 0;
-		// for(String code : program.getCode()){
-		//     int codeSegment = Decoder.decode(code);
-		//     this.memory.setCodeSegment(lineNumber, codeSegment);
-		//     lineNumber++;
-		// }
-
-		// Save changes to registers
-		this.registers.commit();
-
-		// Save changes to memory
-		this.memory.commit();
-	}
-
-	private void step() {
-		// Instruction fetch
-		// Set IF/ID.IR to Memory[PC]
-		registers.setRegister("IF/ID.IR",
-				memory.getCodeSegment((int) (registers.getRegister("PC") / 4)));
-		// Pipeline #2 doesn't have NPC
-
-		// Instruction decode
-		long ID_IR = registers.getRegister("IF/ID.IR"); // previous value
-		registers.setRegister("ID/EX.IR", ID_IR);
-		registers.setRegister("ID/EX.A", ByteUtils.getRS((int) ID_IR));
-		registers.setRegister("ID/EX.B", ByteUtils.getRT((int) ID_IR));
-		registers.setRegister("ID/EX.Imm", ByteUtils.getImm((int) ID_IR));
-
-		// Execute
-
-		// Memory
-
-		// Write back
-
-		registers.commit();
-		memory.commit();
-	}
 }
---
+++ src/simulator/Simulator.java
@@ -0,0 +1,89 @@
+package simulator;
+
+import java.math.BigInteger;
+import java.util.Map.Entry;
+
+import util.ByteUtils;
+import architecture.Memory;
+import architecture.Registers;
+
+public class Simulator {
+	private Memory memory;
+	private Registers registers;
+
+	public Simulator() {
+		this.memory = new Memory();
+		this.registers = new Registers();
+	}
+
+	public void loadProgram(Program program) {
+
+		// Load registers
+		for (Entry<String, Object> entry : program.getRegisters().entrySet()) {
+			String register = entry.getKey();
+			Long value = 0L;
+
+			// We need because YAML loads data based on their size, but we
+			// ALWAYS need a Long value.
+			if (entry.getValue() instanceof Integer)
+				value = new Long((Integer) entry.getValue());
+			else if (entry.getValue() instanceof Long)
+				value = (Long) entry.getValue();
+			else if (entry.getValue() instanceof BigInteger)
+				value = ((BigInteger) entry.getValue()).longValue();
+
+			try {
+				this.registers.setRegister(
+						Integer.parseInt(register.replaceAll("R(\\d+)", "$1")),
+						value);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Load memory
+		for (Entry<Integer, Byte> entry : program.getMemory().entrySet()) {
+			this.memory.setMemoryAddress(entry.getKey(), entry.getValue());
+		}
+
+		// TODO: Place code that saves the code to the code segment here
+		// You can get the code in program.code
+		// int lineNumber = 0;
+		// for(String code : program.getCode()){
+		// int codeSegment = Decoder.decode(code);
+		// this.memory.setCodeSegment(lineNumber, codeSegment);
+		// lineNumber++;
+		// }
+
+		// Save changes to registers
+		this.registers.commit();
+
+		// Save changes to memory
+		this.memory.commit();
+	}
+
+	public void step() {
+		// Instruction fetch
+		// Set IF/ID.IR to Memory[PC]
+		registers.setRegister("IF/ID.IR",
+				memory.getCodeSegment((int) (registers.getRegister("PC") / 4)));
+		// Pipeline #2 doesn't have NPC
+
+		// Instruction decode
+		long ID_IR = registers.getRegister("IF/ID.IR"); // previous value
+		registers.setRegister("ID/EX.IR", ID_IR);
+		registers.setRegister("ID/EX.A", ByteUtils.getRS((int) ID_IR));
+		registers.setRegister("ID/EX.B", ByteUtils.getRT((int) ID_IR));
+		registers.setRegister("ID/EX.Imm", ByteUtils.getImm((int) ID_IR));
+
+		// Execute
+
+		// Memory
+
+		// Write back
+
+		registers.commit();
+		memory.commit();
+	}
+
+}
