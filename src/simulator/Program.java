8ac43cdb39bae45e9e53be2e4218530f31360109
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-12-14 13:04:02 +0800

    Deleted uneccessary stuff

--- src/simulator/Program.java
+++ src/simulator/Program.java
@@ -1,6 +1,5 @@
 package simulator;
 
-import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -16,36 +15,21 @@ public class Program {
 	private ArrayList<String> code;
 
 	public Program(String document) {
-		loadYAML(document);
-	}
-
-//	public Program(FileInputStream document) {
-//		loadYAML(document);
-//	}
-
-	private void loadYAML(Object document) {
 		Yaml yaml = new Yaml();
 
-		if (document instanceof FileInputStream || document instanceof String) {
-			Map map = (Map) yaml.load((String) document);
-//			if (document instanceof FileInputStream)
-//				map = (Map) yaml.load((FileInputStream) document);
-//			else
-//				map = (Map) yaml.load((String) document);
+		Map map = (Map) yaml.load((String) document);
 
-			this.registers = (LinkedHashMap) map.get("registers");
+		this.registers = (LinkedHashMap) map.get("registers");
 
-			this.memory = new HashMap<Integer, Byte>();
-			Map<Integer, Integer> memoryHash = (LinkedHashMap) map
-					.get("memory");
-			for (Entry<Integer, Integer> entry : memoryHash.entrySet()) {
-				this.memory.put(entry.getKey(), entry.getValue().byteValue());
-			}
-
-			// this.memory = (LinkedHashMap) map.get("memory");
-			this.code = new ArrayList<String>(Arrays.asList(((String) map
-					.get("code")).split("\n")));
+		this.memory = new HashMap<Integer, Byte>();
+		Map<Integer, Integer> memoryHash = (LinkedHashMap) map.get("memory");
+		for (Entry<Integer, Integer> entry : memoryHash.entrySet()) {
+			this.memory.put(entry.getKey(), entry.getValue().byteValue());
 		}
+
+		this.code = new ArrayList<String>(Arrays.asList(((String) map
+				.get("code")).split("\n")));
+
 	}
 
 	public Map<String, Object> getRegisters() {
